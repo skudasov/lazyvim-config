6504ad943a67e4c63e8e7780325b4bdbdad18550
skudasov <f4hrenh9it@gmail.com>
2025-03-01 18:38:00 +0100

    init

---
+++ lua/plugins/colorshema.lua
@@ -0,0 +1,12 @@
+-- Default options:
+
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 999,
+    config = function()
+      vim.cmd("colorscheme kanagawa-wave")
+    end,
+  },
+}
