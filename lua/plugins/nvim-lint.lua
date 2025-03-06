315f9f45cfc7a2d6ab7964b7540d804635511e86
skudasov <f4hrenh9it@gmail.com>
2025-03-06 21:30:15 +0100

    working Golang lint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,29 +1,16 @@
 return {
-    "mfussenegger/nvim-lint",
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local lint = require("lint")
-
-        lint.linters_by_ft = {
-            javascript = { "eslint_d" },
-            typescript = { "eslint_d" },
-            vue = { "eslint_d" },
-            go = { "golangci-lint" },
-            python = { "ruff" },
-        }
-
-        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-            group = lint_augroup,
-            callback = function()
-                lint.try_lint()
-            end,
-        })
-
-        vim.keymap.set("n", "<leader>li", function()
-            lint.try_lint()
-        end, { desc = "Trigger linting for current file" })
-    end,
+    {
+        "mfussenegger/nvim-lint",
+        event = "BufReadPre",
+        config = function()
+            require("lint").linters_by_ft = {
+                go = { "golangcilint" },
+            }
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end,
+    },
 }
