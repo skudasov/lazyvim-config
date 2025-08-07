e938d66eadf34c7118f80493e03500f82b843663
skudasov <f4hrenh9it@gmail.com>
2025-08-07 23:35:05 +0200

    diffview

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap.set
+
+-- Diffview keymaps
+keymap("n", "<leader>fh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History (current)" })
+keymap("n", "<leader>fH", "<cmd>DiffviewFileHistory<cr>", { desc = "File History (prompt)" })
+keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
