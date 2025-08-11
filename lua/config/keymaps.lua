8665aafd9fe9f5538f46c9e3ab960cf72990cb5c
skudasov <f4hrenh9it@gmail.com>
2025-08-11 02:36:51 +0200

    themes and remap git history

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,10 @@
 local keymap = vim.keymap.set
 
 -- Diffview keymaps
-keymap("n", "<leader>fh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History (current)" })
-keymap("n", "<leader>fH", "<cmd>DiffviewFileHistory<cr>", { desc = "File History (prompt)" })
+keymap("n", "<leader>gdh", "<cmd>DiffviewFileHistory %<cr>", { desc = "File History (current)" })
+keymap("n", "<leader>gdH", "<cmd>DiffviewFileHistory<cr>", { desc = "File History (prompt)" })
 keymap("n", "<leader>mo", "<cmd>DiffviewOpen<cr>", { desc = "Merge conflict resolution tool" })
 keymap("n", "<leader>mc", "<cmd>DiffviewClose<cr>", { desc = "Close merge conflict resolution tool" })
+-- Oil Keymaps
 keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
